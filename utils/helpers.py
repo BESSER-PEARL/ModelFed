8982882009e89746cdcfd7f73219f271d8765872
ivan-alfonso <ivandaalf@hotmail.com>
2025-02-09 19:47:38 +0100

    Add check_permission function to ensure user has required permissions before processing objects

--- utils/helpers.py
+++ utils/helpers.py
@@ -3,6 +3,8 @@ from besser.BUML.metamodel.structural import (
     BooleanType, FloatType, TimeDeltaType, DateType,
     Multiplicity
 )
+from models import Activity
+from storage import get_object
 
 def map_type(type_str):
     """Parses a string dataType and returns a PrimitiveDataType object"""
@@ -33,3 +35,22 @@ def parse_multiplicity(multiplicity_str: str) -> Multiplicity:
         max_val = parts[1] if parts[1] == "*" else int(parts[1])
 
     return Multiplicity(min_val, max_val)
+
+def check_permission(activity: Activity) -> bool:
+    """Checks if the actor has permission to perform the activity on the target."""
+    domain_model = get_object(id_=str(activity.target))
+    if activity.type == "Create":
+        for grant in domain_model.grants:
+            print(grant.user)
+            print(activity.actor)
+            if grant.user == str(activity.actor) and (grant.role == "admin" or grant.role == "write"):
+                return True
+    elif activity.type == "Update":
+        for grant in domain_model.grants:
+            if grant.user == activity.actor and grant.role == "admin" or "write":
+                return True
+    elif activity.type == "Delete":
+        for grant in domain_model.grants:
+            if grant.user == activity.actor and grant.role == "admin":
+                return True
+    return False
