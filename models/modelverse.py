28d75d11272500f50a2a5dcbc3c540378e7ad886
ivan-alfonso <ivandaalf@hotmail.com>
2025-02-11 18:57:23 +0100

    Create Pydantic classes for Modelverse

--- models/modelverse.py
+++ models/modelverse.py
@@ -1,38 +1,145 @@
-from besser.BUML.metamodel.structural import Element
+from typing import Optional, Union, List, Any, Literal
+from datetime import datetime
+from pydantic import BaseModel, HttpUrl, Field
 
-# Define a backing variables
-private_id = "_id"
-_private_attributed_to = "_attributed_to"
-_private_users = "_users"
+class Object(BaseModel):
+    context: Union[HttpUrl, List[HttpUrl]] = Field(
+        default=[
+            "https://www.w3.org/ns/activitystreams",
+            "https://BESSER-PEARL.github.io/Modelverse/ns/modelverse.jsonld"
+        ],
+        alias="@context"
+    )
+    name: Optional[str] = None
+    type: str
+    id: HttpUrl
+    to: Optional[List[HttpUrl]] = None
 
-# id
-@property
-def id(self):
-    return getattr(self, private_id, None)
+    class Config:
+        fields = {
+            'context': '@context'
+        }
 
-@id.setter
-def id(self, id: str):
-    setattr(self, private_id, id)
+class MvDomainModel(Object):
+    types: Optional[List[Union[Object, HttpUrl]]]
+    packages: Optional[List[Union[Object, HttpUrl]]] = None
+    associations: Optional[List[Union[Object, HttpUrl]]] = None
+    generalizations: Optional[List[Union[Object, HttpUrl]]] = None
 
-# attributed_to
-@property
-def attributed_to(self) -> str:
-    return getattr(self, _private_attributed_to, None)
+class MvModelElement(Object):
+    visibility: Optional[str] = None
 
-@attributed_to.setter
-def attributed_to(self, value: str):
-    setattr(self, _private_attributed_to, value)
+class MvType(MvModelElement):
+    pass
 
-# users
-@property
-def users(self) -> [str]:
-    return getattr(self, _private_users, [])
+class MvDataType(MvType):
+    pass
 
-@users.setter
-def users(self, value: [str]):
-    setattr(self, _private_users, value)
+class MvPrimitiveDataType(MvDataType):
+    pass
 
-# Apply monkey patching in BESSER: Add the property to the Element class
-Element.id = id
-Element.attributed_to = attributed_to
-Element.users = users
+class MvTypedElement(MvModelElement):
+    elementType: Union[
+        MvType,
+        HttpUrl,
+        Literal[
+            'str', 
+            'int', 
+            'float', 
+            'boolean', 
+            'date', 
+            'time', 
+            'datetime', 
+            'timedelta'
+        ]
+    ] = None
+
+class MvEnumerationLiteral(MvModelElement):
+    value: Optional[str] = None
+    owner: Optional[HttpUrl] = None
+
+class MvEnumeration(MvDataType):
+    literals: Optional[List[Union[MvEnumerationLiteral, HttpUrl]]] = None
+
+class MvParameter(MvTypedElement):
+    defaultValue: Optional[Any] = None
+    owner: Optional[HttpUrl] = None
+
+class MvMethod(MvTypedElement):
+    parameters: Optional[List[Union[MvParameter, HttpUrl]]] = None
+    code: Optional[str] = None
+    owner: Optional[HttpUrl] = None
+    isAbstract: Optional[bool] = None
+
+class MvProperty(MvTypedElement):
+    owner: Optional[HttpUrl] = None
+    multiplicity: Optional[str] = None
+    isComposite: Optional[bool] = None
+    isNavigable: Optional[bool] = None
+    isId: Optional[bool] = None
+
+class MvClass(MvType):
+    attributes: Optional[List[Union[MvProperty, HttpUrl]]] = None
+    methods: Optional[List[Union[MvMethod, HttpUrl]]] = None
+    isAbstract: Optional[bool] = None
+
+class MvPackage(MvModelElement):
+    classes: Optional[List[Union[MvClass, HttpUrl]]] = None
+
+class MvAssociation(MvModelElement):
+    ends: Optional[List[Union[MvProperty, HttpUrl]]] = None
+
+class MvBinaryAssociation(MvAssociation):
+    pass
+
+class MvGeneralization(MvModelElement):
+    general: Optional[Union[MvClass, HttpUrl]] = None
+    specific: Optional[Union[MvClass, HttpUrl]] = None
+    isDisjoint: Optional[bool] = None
+    isComplete: Optional[bool] = None
+
+class MvGrant(Object):
+    modelElement: HttpUrl
+    user: HttpUrl
+    role: Literal[
+            'writer', 
+            'admin', 
+            'read', 
+            'maintainer'
+        ]
+
+# Mapping of type to Pydantic classes
+object_type_map = {
+    "Class": MvClass,
+    "PrimitiveDataType": MvPrimitiveDataType,
+    "EnumerationLiteral": MvEnumerationLiteral,
+    "Enumeration": MvEnumeration,
+    "Parameter": MvParameter,
+    "Method": MvMethod,
+    "Property": MvProperty,
+    "Package": MvPackage,
+    "Association": MvAssociation,
+    "BinaryAssociation": MvBinaryAssociation,
+    "Generalization": MvGeneralization,
+    "Grant": MvGrant,
+}
+
+class Activity(Object):
+    actor: HttpUrl
+    object: Union[Object, HttpUrl]
+    target: Optional[HttpUrl] = None
+    timestamp: datetime
+
+    def __init__(self, **data):
+        obj_data = data.get("object")
+
+        if isinstance(obj_data, str) and obj_data.startswith("http"):
+            data["object"] = HttpUrl(obj_data)
+        elif isinstance(obj_data, dict):  
+            obj_type = obj_data.get("type")
+            if obj_type in object_type_map:
+                data["object"] = object_type_map[obj_type](**obj_data)
+            else:
+                data["object"] = Object(**obj_data)
+
+        super().__init__(**data)
