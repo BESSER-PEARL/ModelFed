58195c29f907298c758f93f2dfe6d93c505964d2
ivan-alfonso <ivandaalf@hotmail.com>
2025-02-14 17:10:14 +0100

    move helpers functions to each model slot

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,44 +1,8 @@
-from besser.BUML.metamodel.structural import (
-    StringType, IntegerType, DateTimeType, TimeType,
-    BooleanType, FloatType, TimeDeltaType, DateType,
-    Multiplicity
-)
 from models import Activity
-from storage import get_object, get_grants
-
-def map_type(type_str):
-    """Parses a string dataType and returns a PrimitiveDataType object"""
-    mapping = {
-        "int": IntegerType,
-        "str": StringType,
-        "date": DateType,
-        "datetime": DateTimeType,
-        "time": TimeType,
-        "boolean": BooleanType,
-        "float": FloatType,
-        "timedelta": TimeDeltaType
-    }
-    return mapping.get(type_str, None)
-
-def parse_multiplicity(multiplicity_str: str) -> Multiplicity:
-    """Parses a string representation of multiplicity and returns a Multiplicity object."""
-
-    if multiplicity_str == "*":
-        return Multiplicity(0, "*")
-
-    parts = multiplicity_str.split("..")
-
-    if len(parts) == 1:
-        min_val = max_val = int(parts[0])
-    else:
-        min_val = int(parts[0])
-        max_val = parts[1] if parts[1] == "*" else int(parts[1])
-
-    return Multiplicity(min_val, max_val)
+from storage import get_grants
 
 def check_permission(activity: Activity) -> bool:
     """Checks if the actor has permission to perform the activity on the target."""
-    #domain_model = get_object(id_=activity.target)
     grants = get_grants(activity.target)
     if activity.type == "Create":
         for grant in grants:
