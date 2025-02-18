65035ddb354e730066c303347c7a2b5ed967ca07
ivan-alfonso <ivandaalf@hotmail.com>
2025-02-18 16:38:58 +0100

    Add methods to delete elements in PyEcore

--- model_slot/pyEcore/delete.py
+++ model_slot/pyEcore/delete.py
@@ -6,6 +6,7 @@ from models import (
     MvBinaryAssociation, MvGrant
 )
 from storage import get_object, delete_object, delete_grant
+from model_slot.pyEcore.helpers import find_package_for_class, find_containing_class
 
 def delete_domain_model(obj: MvDomainModel, target: HttpUrl) -> None:
     """Delete a DomainModel."""
@@ -15,57 +16,38 @@ def delete_class(obj: MvClass, target: HttpUrl) -> None:
     """Delete a Class."""
     target_class = get_object(obj.id)
 
-    # Remove class from the domain model
+    # Remove class from the domain model or subpackages
     domain_model = get_object(target)
-    domain_model.types = {typ for typ in domain_model.types if typ != target_class}
+    package_owner = find_package_for_class(domain_model, target_class)
+    package_owner.eClassifiers.remove(target_class)
 
+    delete_object(obj.id)
+
+    # Remove class attributes and references from temporal storage
+    for attr in target_class.eStructuralFeatures:
+        delete_object(attr.id)
+
+
+'''
     # Remove class from generalizations
     for gen in domain_model.generalizations:
         gen.general = None if gen.general == target_class else gen.general
         gen.specific = None if gen.specific == target_class else gen.specific
-
-    # Remove class from packages
-    for package in domain_model.packages:
-        package.classes = {cls for cls in package.classes if cls != target_class}
-
-    # Remove class from properties, methods, and parameters
-    for cls in domain_model.get_classes():
-        if cls.attributes:
-            for attr in cls.attributes:
-                if attr.owner and attr.owner == target_class:
-                    attr.owner = None
-                if attr.type and attr.type == target_class:
-                    attr.type = None
-        if cls.methods:
-            for method in cls.methods:
-                if method.owner and method.owner == target_class:
-                    method.owner = None
-                if method.type and method.type == target_class:
-                    method.type = None
-                if method.parameters:
-                    for param in method.parameters:
-                        if param.type and param.type == target_class:
-                            param.type = None
-
-    # Remove class from associations
-    for asso in domain_model.associations:
-        for end in asso.ends:
-            end.type = None if end.type == target_class else end.type
-
-    delete_object(obj.id)
+'''
 
 def delete_property(obj: MvProperty, target: HttpUrl) -> None:
     """Delete a Property."""
     target_prop = get_object(obj.id)
+    domain_model = get_object(target)
 
-    if target_prop.owner:
-        owner = target_prop.owner
-        if type(owner).__name__ == "Class":
-            owner.attributes = {attr for attr in owner.attributes if attr != target_prop}
-        elif type(owner).__name__ in {"Association", "BinaryAssociation"}:
-            owner.ends = {end for end in owner.ends if end != target_prop}
+    containing_class = find_containing_class(domain_model, target_prop)
+
+    if containing_class:
+        containing_class.eStructuralFeatures.remove(target_prop)
+
     delete_object(obj.id)
 
+'''
 def delete_bin_association(obj: MvBinaryAssociation, target: HttpUrl) -> None:
     """Delete a BinaryAssociation."""
     target_asso = get_object(obj.id)
@@ -157,6 +139,7 @@ def delete_package(obj: MvPackage, target: HttpUrl) -> None:
 
     delete_object(obj.id)
 
+'''
 def remove_grant(obj: MvGrant, target: HttpUrl) -> None:
     """Delete a Grant."""
     delete_grant(obj.id)
@@ -166,13 +149,13 @@ type_handlers = {
     "DomainModel": (delete_domain_model),
     "Class": (delete_class),
     "Property": (delete_property),
-    "BinaryAssociation": (delete_bin_association),
-    "Enumeration": (delete_enumeration),
-    "EnumerationLiteral": (delete_enumeration_literal),
-    "Generalization": (delete_generalization),
-    "Method": (delete_method),
-    "Parameter": (delete_parameter),
-    "Package": (delete_package),
+    # "BinaryAssociation": (delete_bin_association),
+    # "Enumeration": (delete_enumeration),
+    # "EnumerationLiteral": (delete_enumeration_literal),
+    # "Generalization": (delete_generalization),
+    # "Method": (delete_method),
+    # "Parameter": (delete_parameter),
+    # "Package": (delete_package),
     "Grant": (remove_grant),
 }
 
